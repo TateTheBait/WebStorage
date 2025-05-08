67d61fc3c8c3641194e082d25ae24e209f99b7a6
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2025-05-07 20:15:48 -0500

    More TS

--- ScheduleV/script.ts
+++ ScheduleV/script.ts
@@ -1,11 +1,41 @@
 import * as fs from 'fs';
+import * as crypto from 'crypto';
+
+// YOU WILL NOT FIND ANY ORIGINAL MINIGAME FILES FOR MY PAYED SCRIPT. 
+// THIS IS JUST THE INITIAL FRAMEWORK!!
 
 
-const ui = document.getElementById("UI");
+const ui: object = document.getElementById("UI"); 
+
+function encrypt(text) {
+    return CryptoJS.AES.encrypt(text, "WrenchKey1");
+}
+
+function decrypt(text) {
+    return CryptoJS.AES.decrypt(text, "WrenchKey1");
+}
+
+
+
 
 
 function switchPage(path) {
     const file: string = fs.readFileSync(path, 'utf-8');
     ui?.innerHTML = "";
-    ui?.innerHTML = file;
+    decryptedfile = decrypt(file);
+
+    ui?.innerHTML = decryptedfile;
+
+    // Developer Note:
+    // Further Files need to be started not from body, but all of the stuff that goes IN body.
+
+    // Nonetheless this is pretty simple to read, goodluck!
 }
+
+function convertGameToPath(gameName) {
+    const prefix: string = "games/";
+
+    return pathinit + gameName + ".html"; // Return the appended string, very basic but easy to use!
+}
+
+
